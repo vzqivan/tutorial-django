8bca66afbcd57dab00c90864452b0b2345102447
vzqivan <vzqivan@gmail.com>
2017-02-22 11:33:30 -0400

    Now the site is able to edit existing posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-	url(r'^$', views.post_list),
-	url (r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='blog.views.post_detail')
+	url(r'^$', views.post_list, name="blog.views.post_list"),
+	url (r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='blog.views.post_detail'),
+	url(r'^post/new/$', views.post_new, name='blog.views.post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit')
 ]
